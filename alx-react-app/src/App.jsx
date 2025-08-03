e80e70840b3258b90f8806782ef26d7c2f4c9428
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-03 23:20:02 +0100

    add some featurs

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,6 +7,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
@@ -19,6 +20,17 @@ function App() {
 }
 
 export { App }
+
+function App() {
+  return (
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+}
+
+export { App }
+
 
 
 
