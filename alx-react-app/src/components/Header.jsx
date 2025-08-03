5afdd483b4945bbd20408c41b8f75904b166e81d
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-03 22:53:40 +0100

    add some featurs

---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,35 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+
+export default Header;
+
+
+MainContent.jsx
+jsx
+function MainContent() {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
+
+Footer.jsx
+jsx
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
