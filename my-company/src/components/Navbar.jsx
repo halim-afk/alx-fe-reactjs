320ad2793e80276f9447335db0edb88b232c01b6
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-06 12:33:14 +0100

    add features

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+    <nav style={{ display: 'flex', 
+      justifyContent: 'center', backgroundColor: '#333', padding: '10px' }}>
       <Link to="/" style={{ color: 'white', margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ color: 'white', margin: '0 10px' }}>Services</Link>
