c279e03d7f50c42bdd268ab2fe8a81999e2e01ee
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-06 10:58:11 +0100

    add new directotry:alx-react-app-new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,6 +2,8 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f2f2f2', minHeight: '300px' }}>
       <p>Welcome to the main content of the app.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+
     </main>
   );
 }
