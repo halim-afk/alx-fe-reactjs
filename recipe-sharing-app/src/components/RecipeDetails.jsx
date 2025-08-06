33dade165ea6eb5bcfb67a6fc6df21c7749703dc
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-06 13:17:05 +0100

    add Directory: recipe-sharing-app

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -7,4 +7,20 @@ import { useRecipeStore } from './recipeStore';
 const RecipeDetails = () => {
   const { id } = useParams();
   const recipe = useRecipeStore((state) =>
-    state.recipes.find((recipe) => recipe.id === parse
+    state.recipes.find((recipe) => recipe.id === parseInt(id))
+  );
+
+  if (!recipe) {
+    return <p>وصفة غير موجودة.</p>;
+  }
+
+  return (
+    <div>
+      <h2>{recipe.title}</h2>
+      <p>{recipe.description}</p>
+      <p>معرف الوصفة: {recipe.id}</p>
+    </div>
+  );
+};
+
+export default RecipeDetails;
