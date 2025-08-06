f1f4d6d9bf37b9e3144d6045d9cbe506c5b8728a
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-06 11:59:53 +0100

    add featurs

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,45 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
- 
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <Header />
       <MainContent />
       <Footer />
-    </>
-  );
-}
-
-export { App }
-
-function App() {
-  return (
-    <div>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-    </div>
-  );
-}
-
-export { App }
-
-
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-      <WelcomeMessage />
-      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -62,8 +37,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
