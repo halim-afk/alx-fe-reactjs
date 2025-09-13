36016900e27397c66a716aa350314d3f18ac82ca
halim-afk <a.alouah0243@uca.ac.ma>
2025-09-13 12:12:37 +0100

    add /components/searchbar

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>ابدأ في بناء التطبيق من هنا...</p>
+      <SearchBar />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Search GitHub User..." />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
