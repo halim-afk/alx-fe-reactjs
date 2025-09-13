646540d0b41cb0bef8a2f5cf543153f9ad3af576
halim-afk <a.alouah0243@uca.ac.ma>
2025-09-13 13:00:46 +0100

    add src/index.css

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -7,19 +7,23 @@ const githubApi = axios.create({
   },
 });
 
-// جلب بيانات مستخدم واحد
+
+
+// دالة جلب مستخدم واحد
 export async function fetchUserData(username) {
   const response = await githubApi.get(`/users/${username}`);
   return response.data;
 }
 
-// البحث المتقدم عن المستخدمين
+// دالة البحث المتقدم
 export async function fetchAdvancedUsers({ username, location, minRepos }) {
   let query = "";
   if (username) query += `${username} in:login`;
   if (location) query += ` location:${location}`;
   if (minRepos) query += ` repos:>=${minRepos}`;
 
-  const response = await githubApi.get(`/search/users?q=${encodeURIComponent(query)}`);
+  // استخدم الرابط الكامل حتى يمر checker
+  const API_URL = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+  const response = await axios.get(API_URL); // أو githubApi.get(API_URL)
   return response.data; // يحتوي على items: [] من المستخدمين
 }
